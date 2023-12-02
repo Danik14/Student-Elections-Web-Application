a5fee53ff7178aae1abe06a54cc8245f30670392
Danik <chapagana@gmail.com>
2023-12-02 17:38:02 +0600

    wip

--- src/main/java/gigachads/noenemies/diploma/api/security/SecurityConfig.java
+++ src/main/java/gigachads/noenemies/diploma/api/security/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static gigachads.noenemies.diploma.domain.model.user.UserRole.ADMIN;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -15,19 +13,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
-        return http.authorizeHttpRequests(r -> r
-                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/api/v1/auth/login")
-                        .defaultSuccessUrl("/api/v1/loginsuccess")
-                ).logout(l -> l
-                        .logoutSuccessUrl("/api/v1/logoutsucess")
-                        .permitAll()
-                )
+//        return http.authorizeHttpRequests(r -> r
+//                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+//                        .requestMatchers("/api/v1/auth/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2Login(oauth2 -> oauth2
+//                        .loginPage("/api/v1/auth/login")
+//                        .defaultSuccessUrl("/api/v1/loginsuccess")
+//                ).logout(l -> l
+//                        .logoutSuccessUrl("/api/v1/logoutsucess")
+//                        .permitAll()
+//                )
+//
+//                .build();
 
+        return http.authorizeHttpRequests(r -> r
+                                        .anyRequest().authenticated()
+                )
                 .build();
 
     }
