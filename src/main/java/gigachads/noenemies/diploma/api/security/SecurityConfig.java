923512a72351ffa037d22fa77ebf768aaee759d3
Danik <chapagana@gmail.com>
2024-01-29 10:11:18 +0600

    wip

--- src/main/java/gigachads/noenemies/diploma/api/security/SecurityConfig.java
+++ src/main/java/gigachads/noenemies/diploma/api/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 @Configuration
 @EnableWebSecurity
-@Profile(value = {"development", "production"})
 public class SecurityConfig {
 
     @Bean
