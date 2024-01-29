7af08c08da4b0f23e7b50809bf594395fafa3751
Danik <chapagana@gmail.com>
2024-01-29 19:10:29 +0600

    wip

--- src/integration-test/java/gigachads/noenemies/diploma/api/controller/ElectionControllerIT.java
+++ src/integration-test/java/gigachads/noenemies/diploma/api/controller/ElectionControllerIT.java
@@ -20,32 +20,32 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Sql(scripts = "/test_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-@ExtendWith(ContainerHolder.class)
+//@AutoConfigureMockMvc
+//@ActiveProfiles("test")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@Sql(scripts = "/test_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+//@ExtendWith(ContainerHolder.class)
 public class ElectionControllerIT {
 
-    @Autowired
-    private TestRestTemplate template;
-
-    private Map<String, String> singleParam(String key, String value) {
-        Map<String, String> params = new HashMap<>();
-        params.put(key, value);
-        return params;
-    }
-
-    @Test
-    @DisplayName("Testing to finding user details by id")
-    @SneakyThrows
-    void findUserByIdTest() {
-        ResponseEntity<ElectionResponse> response = template.getForEntity("/api/v1/elections/{id}", ElectionResponse.class, singleParam("id", "813a19ae-49ce-4934-917c-78d2d7dc6153"));
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-//        assertThat(response.getBody())
-//                .extracting(UserDTO::getEmail, UserDTO::getFirstName, UserDTO::getLastName)
-//                .containsExactly("user2@some.com", "John", "Duke");
-    }
+//    @Autowired
+//    private TestRestTemplate template;
+//
+//    private Map<String, String> singleParam(String key, String value) {
+//        Map<String, String> params = new HashMap<>();
+//        params.put(key, value);
+//        return params;
+//    }
+//
+//    @Test
+//    @DisplayName("Testing to finding user details by id")
+//    @SneakyThrows
+//    void findUserByIdTest() {
+//        ResponseEntity<ElectionResponse> response = template.getForEntity("/api/v1/elections/{id}", ElectionResponse.class, singleParam("id", "813a19ae-49ce-4934-917c-78d2d7dc6153"));
+//
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//
+////        assertThat(response.getBody())
+////                .extracting(UserDTO::getEmail, UserDTO::getFirstName, UserDTO::getLastName)
+////                .containsExactly("user2@some.com", "John", "Duke");
+//    }
 }
